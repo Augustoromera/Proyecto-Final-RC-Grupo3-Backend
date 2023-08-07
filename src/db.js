a8cf2b63602758f9b7ca8d32bc22e14b1c1d7c3a
Paulo Srur <paulopepesrur@gmail.com>
2023-08-07 02:57:40 -0300

    cambios en archivo db para conexion con base de datos

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () =>{
     try {
-      await mongoose.connect('mongodb://localhost/back45-group3');
+      await mongoose.connect('mongodb+srv://Group3:0Q0cWEOrXOCvAeWJ@backgroup3.baggvkl.mongodb.net/');
       console.log(">>>> DB is connected");
     
   } catch (error) {
