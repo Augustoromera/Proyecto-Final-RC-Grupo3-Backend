521519e4f35f67ef70dd7e6008813723e72c143a
Paulo Srur <paulopepesrur@gmail.com>
2023-08-07 02:24:47 -0300

    creando archivo de models y schema

--- src/db.js
+++ src/db.js
@@ -1,1 +1,11 @@
 import mongoose from 'mongoose';
+
+export const connectDB = async () =>{
+    try {
+      await mongoose.connect('mongodb://localhost/back45-group3');
+      console.log(">>>> DB is connected");
+    
+  } catch (error) {
+      console.log(error);
+  }
+};
---
+++ src/models/user.model.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+mongoose.Schema({
+    username:{
+        type: String
+    },
+    
+    email:{
+        type: String
+    },
+
+    password:{
+        type: String
+    }
+})
