9627070d9cbf49ed0e13537c922e9e3743735c00
Paulo Srur <paulopepesrur@gmail.com>
2023-08-07 17:41:44 -0300

    logout terminado

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -71,6 +71,9 @@ export const login = async (req, res) =>{
 
 
 export const logout =  (req, res) =>{
-    res.cookie("token", "")
+    res.cookie("token", "",{
+        expires: new Date(0)
+    })
+    return res.sendStatus(200);
 
 }
