1cbe6727f1b0773ba271b58978f18ca9811558bb
Paulo Srur <paulopepesrur@gmail.com>
2023-08-07 16:18:26 -0300

    importando y probando jsonWebToken

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 
 export const register = async (req, res) =>{
@@ -18,10 +19,19 @@ const {email, password, username} = req.body;
      })
     
      const userSaved =  await newUser.save();
+
+      jwt.sign({
+        id: userSaved._id,
+      }, 'secret123', 
+      
+      {})
+
        res.json({
         id: userSaved._id,
         username: userSaved.username,
         email: userSaved.email,
+        createdAt: userSaved.createdAt,
+        updateAt: userSaved.updateAt,
        });
     
  } catch (error) {
