cecd52f09f136a83aceb2c58b67383f4b3e9adfd
Paulo Srur <paulopepesrur@gmail.com>
2023-08-07 17:32:06 -0300

    finalizando login y creacion de logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -37,6 +37,40 @@ const {email, password, username} = req.body;
 
 
 
-export const login = (req, res) => {
+export const login = async (req, res) =>{
+
+    const {email, password} = req.body;
+    
+     try {
+    
+      const userFound = await User.findOne({ email });
+
+      if(!userFound) return res.status(400).json({message:'user not found'});
+
+        const isMatch= await bcrypt.compare(password, userFound.password)
+    
+         if(!isMatch) return res.status(400).json({message: 'el email o la contraseña son incorrectos'});
+    
+         const token = await createAccessToken({id: userFound._id})
+         res.cookie('token', token);
+          res.json({
+             id: userFound._id,
+             username: userFound.username,
+             email: userFound.email,
+             createdAt: userFound.createdAt,
+             updateAt: userFound.updateAt,
+            });
+        
+     } catch (error) {
+        res.status(500).json({message:error.message});
+     }
+    
+    
+    };
+
+
+
+export const logout =  (req, res) =>{
+    res.cookie("token", "")
 
 }
