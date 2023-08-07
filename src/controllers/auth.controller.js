2d740e67db8127f8e20560f097aa338a02e38b90
Paulo Srur <paulopepesrur@gmail.com>
2023-08-07 15:41:19 -0300

    modificaciones en usuario de modelo y creando newUser

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,2 +1,22 @@
-export const register = (req, res) => res.send('register')
-export const login = (req, res) => res.send('login');
+import User from '../models/user.model.js';
+
+
+export const register = (req, res) =>{
+
+const {email, password, username} = req.body;
+
+ const newUser = new User({
+    username,
+    email,
+    password,
+ })
+
+res.send("registrando")
+
+}
+
+
+
+export const login = (req, res) => {
+
+}
