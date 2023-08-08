3273ff7d862127b58c25d494ac572e78069e62bb
Paulo Srur <paulopepesrur@gmail.com>
2023-08-08 00:59:37 -0300

    task model terminado

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -2,7 +2,9 @@ import Task from '../models/task.model.js';
 
 
 export const getTasks = async (req,res) =>{
-    const tasks = await Task.find()
+    const tasks = await Task.find({
+        user: req.user.id
+    }).populate('user');
     res.json(tasks);
 
 
@@ -11,11 +13,15 @@ export const getTasks = async (req,res) =>{
 export const createTask = async (req,res) =>{
 
    const { title, description, date} = req.body
+
+    console.log(req.user)
+
    const newTask = new Task({
     title,
     description,
     date,
-   })
+    user : req.user.id,
+   });
 
    const savedTask = await newTask.save()
    res.json(savedTask);
@@ -23,7 +29,7 @@ export const createTask = async (req,res) =>{
 };
 
 export const getTask = async (req,res) =>{
-    const task = await Task.findById(req.params.id);
+    const task = await Task.findById(req.params.id).populate('user');
     if(!task) return res.status(404).json({message: "task not found"});
     res.json(task); 
 
@@ -32,7 +38,7 @@ export const getTask = async (req,res) =>{
 export const deleteTask = async (req,res) =>{
     const task = await Task.findByIdAndDelete(req.params.id);
     if(!task) return res.status(404).json({message: "task not found"});
-    res.json(task);
+    return res.sendStatus(204)
 };
 
 export const updateTask = async (req,res) =>{
--- src/models/task.model.js
+++ src/models/task.model.js
@@ -16,6 +16,8 @@ const taskSchema = new mongoose.Schema({
     },
     user:{
         type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
     }
 },{
     timestamps: true,
