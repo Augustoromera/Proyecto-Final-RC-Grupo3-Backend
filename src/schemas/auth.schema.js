9db62224e747938f73390040ecae2bc36f6f7bc1
Paulo Srur <paulopepesrur@gmail.com>
2023-08-08 02:00:49 -0300

    validacion de schema terminada

--- src/schemas/auth.schema.js
+++ src/schemas/auth.schema.js
@@ -2,16 +2,16 @@ import {z} from "zod";
 
 export const registerSchema = z.object({
     username : z.string({
-        required_error:'Username is required'
+        required_error:'El nombre de usuario es obligatorio'
     }),
     email: z.string({
-        required_error:'Email is required'
+        required_error:'El email es obligatorio'
     }).email({
-        message: "Invalid email"
+        message: "Email invalido"
     }),
 
     password: z.string({
-        required_error:'Password is required'
+        required_error:'La contraseña es obligatoria'
     }).min(6, {
         message: "La contraseña debe tener al menos 6 caracteres"
     })
