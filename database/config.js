56117da9b9d6013bd79b032a2b9def197d3abe6d
Paulo Srur <paulopepesrur@gmail.com>
2023-08-29 14:27:38 -0300

    agregue base de datos de Augusto para conectar

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const dbConnection = async () => {
     try {
-        await mongoose.connect(process.env.DB_CNN);
+        await mongoose.connect(process.env.DB_CNN) || 'mongodb+srv://augusto:wallaby42@proyectoweb.e2tzutv.mongodb.net/';
         console.log('conectado a la base de datos');
     } catch (error) {
         console.log('error');
