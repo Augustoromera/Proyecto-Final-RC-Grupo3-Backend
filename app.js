3a7b310d812551b94d6184628cd3d94299aabc3f
Paulo Srur <paulopepesrur@gmail.com>
2023-08-28 21:59:27 -0300

    base de datos cambio de puerto

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ import mongoose from 'mongoose';
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect(process.env.DB_CNN || 'mongodb+srv://Group3:0Q0cWEOrXOCvAeWJ@backgroup3.baggvkl.mongodb.net');
+        await mongoose.connect(process.env.DB_CNN);
         console.log('Conectado a la base de datos');
     } catch (error) {
         console.log('Error de conexión a la base de datos:', error);
